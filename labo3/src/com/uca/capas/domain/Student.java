77c013b9e8b63d177defaddbec8ddc15c8c00f77
UCA <UCA@DEILAB02-PC16>
2019-04-24 11:11:54 -0600

    labo3

---
+++ labo3/src/com/uca/capas/domain/Student.java
@@ -0,0 +1,13 @@
+package com.uca.capas.domain;
+
+public class Student {
+	
+	public String name;
+	public String lstName;
+	public String bDate;
+	public String career;
+	public String experience;
+	
+	
+
+}
